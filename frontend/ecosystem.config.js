f8be928f6f1ed96462dbe2dcfb080280979447a2
Ivan Shanin <ivanxshanin@gmail.com>
2024-09-02 20:31:47 +0300

    add frontend deploy pipeline

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,29 @@
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './.env.deploy' });
+
+const {
+  SSH_OPTIONS,
+  DEPLOY_USER,
+  DEPLOY_HOST,
+  DEPLOY_PATH,
+  DEPLOY_REPO,
+  DEPLOY_REF,
+} = process.env;
+
+module.exports = {
+  deploy: {
+    production: {
+      ssh_options: SSH_OPTIONS,
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      path: DEPLOY_PATH,
+      repo: DEPLOY_REPO,
+      ref: DEPLOY_REF,
+      'pre-deploy-local':
+        `bash scripts/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST} ${DEPLOY_PATH}`,
+      'post-deploy':
+        'cd frontend && pwd && npm ci && npm run build',
+    },
+  },
+};
