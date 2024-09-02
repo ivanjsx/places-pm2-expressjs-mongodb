33955ecf45b20bc27bebcee3960ae8f5cb0411b8
Ivan Shanin <ivanxshanin@gmail.com>
2024-09-02 23:02:32 +0300

    configure cors & api url

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,7 +4,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
 import { errors } from 'celebrate';
-// import cors from 'cors';
+import cors from 'cors';
 import errorHandler from './middlewares/error-handler';
 import { PORT, DB_ADDRESS } from './config';
 import routes from './routes';
@@ -13,7 +13,7 @@ const app = express();
 mongoose.connect(DB_ADDRESS);
 
 // Только для локальных тестов. Не используйте это в продакшене
-// app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
